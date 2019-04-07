b3a8a0c7e5894f9a4f233810a3891082054410f6
pina14 <a42121@alunos.isel.pt>
2019-04-07 03:19:55 +0100

    Log of exceptions

--- src/main/kotlin/pt/isel/daw/g8/projectmanager/middleware/ApiHandlerExceptionResolver.kt
+++ src/main/kotlin/pt/isel/daw/g8/projectmanager/middleware/ApiHandlerExceptionResolver.kt
@@ -5,15 +5,19 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import pt.isel.daw.g8.projectmanager.loggerFor
 import pt.isel.daw.g8.projectmanager.model.errorModel.ErrorModel
 import pt.isel.daw.g8.projectmanager.model.errorModel.ProblemJson
 import pt.isel.daw.g8.projectmanager.model.errorModel.errorRepresentations.HttpErrorException
+
+private val log = loggerFor<ApiHandlerExceptionResolver>()
 
 @RestControllerAdvice
 class ApiHandlerExceptionResolver : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(Throwable::class)
     fun handleAll(ex: Exception): ResponseEntity<ErrorModel> {
+        log.error(ex.message, ex)
         val error = ProblemJson(
                 title = HttpStatus.INTERNAL_SERVER_ERROR.reasonPhrase,
                 status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -26,6 +30,7 @@ class ApiHandlerExceptionResolver : ResponseEntityExceptionHandler() {
     @ExceptionHandler(HttpErrorException::class)
     fun handleHttpErrorGeneric(ex : HttpErrorException) : ResponseEntity<ErrorModel> {
         val error =  ex.toProblemJson()
+        log.warn(error.detail, ex)
         return ResponseEntity
                 .status(error.status)
                 .header("Content-Type", error.getMediaType())
--- src/main/kotlin/pt/isel/daw/g8/projectmanager/middleware/AuthenticationInterceptor.kt
+++ src/main/kotlin/pt/isel/daw/g8/projectmanager/middleware/AuthenticationInterceptor.kt
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter
 import pt.isel.daw.g8.projectmanager.ProjectPaths
+import pt.isel.daw.g8.projectmanager.loggerFor
 import pt.isel.daw.g8.projectmanager.model.errorModel.errorRepresentations.BadRequestException
 import pt.isel.daw.g8.projectmanager.model.errorModel.errorRepresentations.NotAuthenticatedException
 import pt.isel.daw.g8.projectmanager.model.errorModel.errorRepresentations.WrongCredentialsException
@@ -13,42 +14,49 @@ import java.util.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
+private val log = loggerFor<AuthenticationInterceptor>()
+
 @Component
 class AuthenticationInterceptor : HandlerInterceptorAdapter() {
 
     @Autowired lateinit var userRepo : UserInfoRepo
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        (handler as? HandlerMethod)?.method?.let {m ->
-            if (m.isAnnotationPresent(RequiresAuthentication::class.java)
-                    || m.declaringClass.isAnnotationPresent(RequiresAuthentication::class.java)){
-                val authentication = request.getHeader("Authorization")
-                        ?: throw NotAuthenticatedException()
+        try {
+            (handler as? HandlerMethod)?.method?.let { m ->
+                if (m.isAnnotationPresent(RequiresAuthentication::class.java)
+                        || m.declaringClass.isAnnotationPresent(RequiresAuthentication::class.java)) {
+                    val authentication = request.getHeader("Authorization")
+                            ?: throw NotAuthenticatedException()
 
-                val auth : List<String>
-                val authToDecode = authentication.removePrefix("Basic").trim()
-                try {
-                    auth = String(Base64
-                            .getDecoder()
-                            .decode(authToDecode))
-                            .trim()
-                            .split(":")
-                } catch (ex : IllegalArgumentException) {
-                    throw BadRequestException("Authentication has to encoded in Base64 format.")
+                    val auth: List<String>
+                    val authToDecode = authentication.removePrefix("Basic").trim()
+                    try {
+                        auth = String(Base64
+                                .getDecoder()
+                                .decode(authToDecode))
+                                .trim()
+                                .split(":")
+                    } catch (ex: IllegalArgumentException) {
+                        throw BadRequestException("Authentication has to encoded in Base64 format.")
+                    }
+
+                    if (authToDecode.isEmpty() || auth.size != 2)
+                        throw BadRequestException("Authentication header has to be in format 'Basic <username>:<password>'.")
+
+                    val username = auth[0]
+                    val password = auth[1]
+
+                    val user = userRepo.findById(username)
+                    if (!user.isPresent || user.get().password != password)
+                        throw WrongCredentialsException()
+
+                    request.setAttribute(ProjectPaths.USERNAME_VAR, username)
                 }
-
-                if(authToDecode.isEmpty() || auth.size != 2)
-                    throw BadRequestException("Authentication header has to be in format 'Basic <username>:<password>'.")
-
-                val username = auth[0]
-                val password = auth[1]
-
-                val user = userRepo.findById(username)
-                if (!user.isPresent || user.get().password != password)
-                    throw WrongCredentialsException()
-
-                request.setAttribute(ProjectPaths.USERNAME_VAR, username)
             }
+        } catch (e : Throwable) {
+            log.info("User failed to authenticate.")
+            throw e
         }
 
         return true
