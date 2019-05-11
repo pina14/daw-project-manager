23d19a9617c14b83d756173874920802f6564fe3
pina14 <a42121@alunos.isel.pt>
2019-05-11 23:22:20 +0100

    Enabled CORS in API

--- API/src/main/kotlin/pt/isel/daw/g8/projectmanager/middleware/MvcConfig.kt
+++ API/src/main/kotlin/pt/isel/daw/g8/projectmanager/middleware/MvcConfig.kt
@@ -2,6 +2,7 @@ package pt.isel.daw.g8.projectmanager.middleware
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -10,6 +11,10 @@ class MvcConfig : WebMvcConfigurer {
 
     @Autowired
     private lateinit var authInterceptor: AuthenticationInterceptor
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(authInterceptor)
